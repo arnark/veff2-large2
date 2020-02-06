724057410cafb60371e5e19af478263e56b5a7dd
Arnarkj <arnark18@ru.is>
2020-02-06 08:19:51 +0000

    Added base html, style and some draw functions (draw, undo)

---
+++ drawio.js
@@ -0,0 +1,72 @@
+// Initialize draw and undo stacks
+let drawStack = [];
+let undoStack = [];
+
+// Find the canvas element and set the size
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight - 100;
+
+let mouse = {x: 0, y: 0};
+
+// Update mouse position on mousemove
+canvas.addEventListener('mousemove', function(e) {
+  mouse.x = e.pageX - this.offsetLeft;
+  mouse.y = e.pageY - this.offsetTop;
+}, false);
+
+// Draw tool settings
+ctx.lineWidth = 10;
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.strokeStyle = '#000';
+
+canvas.addEventListener('mousedown', function(e) {
+	canvas.drawStroke = [];
+	canvas.drawDot = { fromX: mouse.x, fromY: mouse.y }
+    ctx.beginPath();
+    ctx.moveTo(mouse.x, mouse.y);
+ 	onPaint();
+    canvas.addEventListener('mousemove', onPaint, false);
+}, false);
+ 
+canvas.addEventListener('mouseup', function() {
+    canvas.removeEventListener('mousemove', onPaint, false);
+    drawStack.push(canvas.drawStroke);
+}, false);
+
+var onPaint = function() {
+
+    ctx.lineTo(mouse.x, mouse.y);
+    ctx.stroke();
+
+    // Push the drawDot to the drawStroke array
+    canvas.drawDot.toX = mouse.x;
+    canvas.drawDot.toY = mouse.y;
+	canvas.drawStroke.push(canvas.drawDot);
+
+	// Reset drawDot
+	canvas.drawDot = { fromX: mouse.x, fromY: mouse.y };
+};
+
+var clearCanvas = function() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+var rePaint = function() {
+	drawStack.forEach(stroke => {
+		stroke.forEach(dot => {
+  			ctx.beginPath();
+    		ctx.moveTo(dot.fromX, dot.fromY);
+    		ctx.lineTo(dot.toX, dot.toY);
+    		ctx.stroke();
+		});
+	});
+};
+
+var undo = function() {
+	drawStack.pop();
+	clearCanvas();
+	rePaint();
+}
